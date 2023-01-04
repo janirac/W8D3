cdb5f3bb32b1c5ee2430be7ea522e678bcbe467e
Janira Crispin <janiracrispin@gmail.com>
2023-01-04 16:15:14 -0500

    my map

--- javascriptd1/skeleton/phase_2_enumerable.js
+++ javascriptd1/skeleton/phase_2_enumerable.js
@@ -1,15 +1,23 @@
 Array.prototype.myEach = function (callback) {
-    emptyList = []
     for (let i = 0; i < this.length; i++) {
         emptyList.push(callback(this[i]));
     }
-    return emptyList
+    return this
 };
 
 function myFunction(num) {
     return num += 1;
 };
 // console.log(typeof myEach)
-console.log([1, 4, 5, 6, 3].myEach(myFunction));
+// console.log([1, 4, 5, 6, 3].myEach(myFunction));
 // console.log("Hello")
 
+Array.prototype.myMap = function (callback) {
+    emptyList = []
+
+    this.myEach(function(element) {
+        emptyList.push(callback(element));
+    })
+
+    return emptyList
+}   //comment 
